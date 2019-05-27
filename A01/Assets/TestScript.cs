ec57984af453ee0da14ce440fab47ef10ecb3e56
unknown <tttan@apcs.vn>
2019-05-27 11:12:19 -0700

    A01

---
+++ A01/Assets/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public Vector2 newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (true)
+        {
+            GetComponent<Transform>().position = newPos;
+            newPos.x += 0.1f;
+        }
+    }
+}
+
+
