69f27a7e1f7b58e16c035964c76a37a8da1fe9d3
unknown <tttan@apcs.vn>
2019-05-31 10:58:14 -0700

    AO2

---
+++ A02/Assets/myobject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myobject : MonoBehaviour
+{
+    public Transform red;
+    public Transform green;
+    public Transform blue;
+    public Transform yellow;
+    public Transform purple;
+    public Transform orange;
+    // Start is called before the first frame update
+    void Start()
+    {
+       for (int i = -6; i < 7; i += 2)
+        {
+                Instantiate(red, new Vector3(i, 3.0f, 0), red.rotation);
+                Instantiate(green, new Vector3(i, 1.5f, 0), green.rotation);
+                Instantiate(blue, new Vector3(i, 0, 0), blue.rotation);
+                Instantiate(yellow, new Vector3(i, -1.5f, 0), yellow.rotation);
+                Instantiate(orange, new Vector3(i, -3.0f, 0), orange.rotation);
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
