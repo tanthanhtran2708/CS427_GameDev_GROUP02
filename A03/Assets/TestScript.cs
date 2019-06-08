ef849a3ded3e5b08bf8da3f3651237dacfbff492
unknown <tttan@apcs.vn>
2019-06-07 20:27:30 -0700

    A03

---
+++ A03/Assets/TestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float jumpPower = 150f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+           // GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+            //GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up*jumpPower);
+        }
+    }
+}
+
+
